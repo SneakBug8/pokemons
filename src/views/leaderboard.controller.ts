c00c46231f6c64c7642e261855e64245198f567d
SneakBug8 <sneakbug8@gmail.com>
2019-07-08 23:04:05 +0300

    passworded leaderboard

--- src/views/leaderboard.controller.ts
+++ src/views/leaderboard.controller.ts
@@ -9,8 +9,15 @@ export class LeaderboardController
     constructor(private readonly leaderboardService: LeaderboardService) { }
 
     @Get("leaderboard")
-    private async handleRequest(@Res() res: express.Response)
+    private async handleRequest(@Req() req: express.Request, @Res() res: express.Response)
     {
+        // test:test
+        if (req.headers.authorization !== "Basic dGVzdDp0ZXN0") {
+            // tslint:disable-next-line: quotemark
+            res.setHeader("WWW-Authenticate", 'Basic realm="Pokemons"');
+            return res.status(401).send("Authentication required."); // Access denied.
+        }
+
         const leaderboard = await this.leaderboardService.GetLeaderboard();
 
         if (!leaderboard) {
