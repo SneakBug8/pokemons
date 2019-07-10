c42dde69f21c65dfe3a5980045ff980813b7b4db
SneakBug8 <sneakbug8@gmail.com>
2019-07-10 13:09:26 +0300

    admin

--- src/views/admin.controller.ts
+++ src/views/admin.controller.ts
@@ -8,9 +8,16 @@ export class LeaderboardController
 {
     constructor(private readonly leaderboardService: LeaderboardService) { }
 
-    @Get("leaderboard")
+    @Get("admin")
     private async handleRequest(@Req() req: express.Request, @Res() res: express.Response)
     {
+        // test:test
+        if (req.headers.authorization !== "Basic dGVzdDp0ZXN0") {
+            // tslint:disable-next-line: quotemark
+            res.setHeader("WWW-Authenticate", 'Basic realm="Pokemons"');
+            return res.status(401).send("Authentication required."); // Access denied.
+        }
+
         const leaderboard = await this.leaderboardService.GetLeaderboard();
 
         if (!leaderboard) {
@@ -21,9 +28,8 @@ export class LeaderboardController
 
         console.log(leaderboard);
 
-        res.render("leaderboard", {
-            leaderboard,
-            hideleaderboardlink: 1
+        res.render("admin", {
+            leaderboard
         });
     }
 }
